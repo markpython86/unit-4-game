69a4432d0839275dbbdcdb870ba58866c0531c91
Mark Faraj <User49@CAPSCI.LOCAL>
2018-10-24 08:57:17 -0500

    Javascript code is done need to work on html and style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,182 +1,91 @@
 $(document).ready(function() {
-	var clearArr = []
-
 
 	var totalScore = 0;
-
-	var randomNumber;
-
 	var win;
-
 	var lose;
+	var winText = 0;
+	var loseText = 0;
+	var a = Math.floor(Math.random() * 12)+1; 
+	var b = Math.floor(Math.random() * 12)+1; 
+	var c = Math.floor(Math.random() * 12)+1; 
+	var d = Math.floor(Math.random() * 12)+1; 
 
-	var gold;
-	var red;
-	var blue;
-	var green;
-
-	// var crystals = ["red", "blue", "gold", "green"];
-
-	function crystals(){
-		// arr.length = totalScore;
-		// totalScore = 0
-		// var arr = [];
-		// arr.length = totalScore;
-		// for (var i = 0; i < 4; i++) {
-		// 	var arr = [];
-		// 	i = Math.floor(Math.random() * 12)+1; 
-		// 	arr.push(i);
-			
-		// }
-		a = Math.floor(Math.random() * 12)+1; 
-		b = Math.floor(Math.random() * 12)+1; 
-		c = Math.floor(Math.random() * 12)+1; 
-		d = Math.floor(Math.random() * 12)+1; 
+	var randomNumber = Math.floor(Math.random() * (120-19))+19;
+	$(".randomNumber").text(randomNumber);
+	function crystals(){		
+		console.log(a);
+		console.log(b);
+		console.log(c);
+		console.log(d);
 
 		$('#red').click(function(){
-		// win = true;
-		// this.value = arr[3];
-		// $('#red').attr("value", (Math.floor(Math.random() * 12) + 1));
-		// console.log(this.value)
 		parseInt(totalScore+= d);
 		console.log(d);
-		$("#totalScore").val(totalScore);
-	    // updateNumberToBEMatched();
+		$("#totalScore").text(totalScore);
 	    checkWin();
-	    // checkLose();
-
 		});
 		$('#blue').click(function(){
-			// this.value = arr[0];
-			// $('#blue').attr("value", (Math.floor(Math.random() * 12) + 1));
 			parseInt(totalScore += a);
 			console.log(a);
 			$("#totalScore").text(totalScore);
-	    checkWin();
-	    // checkLose();
+	    	checkWin();
 		});
 		$('#gold').click(function(){
-			// this.value = arr[1];
-			 // $('#gold').attr("value", (Math.floor(Math.random() * 12) + 1));
 			parseInt(totalScore+= b);
 			console.log(b);
 			$("#totalScore").text(totalScore);
-	    checkWin();
-	    // checkLose();
+	    	checkWin();
 		});
 		$('#green').click(function(){
-			// lose = true;
-			this.value = "";
-			// x = $('#blue').attr("value", (Math.floor(Math.random() * 12) + 1));
 			parseInt(totalScore+= c);
 			console.log(c);
 			$("#totalScore").text(totalScore);
-			// updateNumberToBEMatched();
 	    checkWin();
-	    // checkLose();
 		});
-			// checkWin();
-			// checkLose();
-			// console.log(arr);
-	// 		console.log(this);
-			
-	// 		console.log(this);
-	// 		console.log(this);
-	// 		console.log(this);
 
 	}
 
 	function resetScore(){
-		// arr.length = 0;
 		totalScore = 0;
 		$("#totalScore").text(totalScore);
-		
+		a = Math.floor(Math.random() * 12)+1; 
+		b = Math.floor(Math.random() * 12)+1; 
+		c = Math.floor(Math.random() * 12)+1; 
+		d = Math.floor(Math.random() * 12)+1; 
+		console.log(a);
+		console.log(b);
+		console.log(c);
+		console.log(d);		
 	}
-	// randomNumber = Math.floor(Math.random() * (120-19))+19;
-	// 	console.log(arr);
-	// console.log(randomNumber);
-
-
-	// function updateNumberToBEMatched(){
-	// 	if (win === true || lose ===true){
-	// 		randomNumber = Math.floor(Math.random() * (120-19))+19;
-	// 		$(".randomNumber").text(randomNumber);
-	// 		resetScore();
-	// 		// crystals();
-			
-
-
-
-	// 	}
-	// }
 
 	function updateScreen(){
+		if (win === true || lose ===true){
 		randomNumber = Math.floor(Math.random() * (120-19))+19;
 		$(".randomNumber").text(randomNumber);
-		// $("#totalScore").text("0");
-		resetScore();
-
-		crystals();
-	}
-
-
-	function checkWin(){
-		if (totalScore === randomNumber){
-			// arr = [];
-			console.log("youwin!!!");
-			updateScreen();
-			// crystals();
-			// resetScore();
-		} else if (totalScore >= randomNumber){
-			// arr = [];
-			console.log("lose!!!");
-			updateScreen();
-			// crystals();
-			// resetScore();
 		}
 	}
-
-	// function checkLose(){
-	// 	if (totalScore >= randomNumber){
-	// 		// arr = [];
-	// 		console.log("lose!!!");
-	// 		updateScreen();
-	// 		// crystals();
-	// 		// resetScore();
-	// 	}
-	// }
-	// // function action(){
-	// 	if (red === true){
-	// 		red();
-	// 	}else if (blue === true){
-	// 		blue();
-	// 	}else if (gold === true){
-	// 		gold();
-	// 	}else if (green === true){
-	// 		green();
-	// 	}
-	// }
-
-
-
-
-
-
+	function checkWin(){
+		if (totalScore === randomNumber){
+			console.log("youwin!!!");
+			win = true;
+			winText++;
+			console.log("wins"+winText);
+			$(".wins").text("Wins: "+winText);
+			resetScore();
+			updateScreen();
+		} else if ( randomNumber <= totalScore){
+			console.log("lose!!!");
+			lose = true;
+			resetScore();
+			loseText++;
+			$(".loses").text("Loses: "+loseText);
+			console.log("loses" +loseText);
+			updateScreen();
+		}
+	}
 	updateScreen();
-	// crystals();
+	crystals();
 
 
 
 });
-
-// var e = Math.floor(Math.random() * 120) +19;
-
-
-
-// console.log(arr[Math.floor(Math.random() * 4)]);
-// console.log(arr[Math.floor(Math.random() * 4)]);
-// console.log(arr[Math.floor(Math.random() * 4)]);
-// console.log(arr[Math.floor(Math.random() * 4)]);
-
-
-
